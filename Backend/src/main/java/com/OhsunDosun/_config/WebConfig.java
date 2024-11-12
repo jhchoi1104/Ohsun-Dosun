8384a5ac486a0d3100843676b1478318bf1d3275
jhchoi1104 <163481864+jhchoi1104@users.noreply.github.com>
2024-11-12 10:55:20 +0900

    refactor: 필요 없는 디렉토리/파일 삭제
    삭제 항목: common, home, member, security 관련, test 코드, 파일 업로드 관련

--- Backend/src/main/java/com/OhsunDosun/_config/WebConfig.java
+++ Backend/src/main/java/com/OhsunDosun/_config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[] { RootConfig.class, SecurityConfig.class };
+        return new Class[] { RootConfig.class };
     }
 
     @Override
