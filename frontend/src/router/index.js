c2bacadf80811ca13b8510def02181ead3d78c79
ParkJeongBin <areoin@naver.com>
2024-11-14 21:09:46 +0900

    feat:Chat.vue,Hearder creat

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Main from '@/pages/main/Main.vue'; // 메인페이지
-import List from '@/pages/list/List.vue'; //조회페이지
+import List from '@/pages/list/List.vue'; // 조회페이지
+import Chat from '@/pages/chat/Chat.vue'; // 말하기페이지
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,11 +13,17 @@ const router = createRouter({
       component: Main,
     },
     {
-      //조회페이지
-      path:'/list',
-      name:'List',
-      component:List,
-    }
+      // 조회페이지
+      path: '/list',
+      name: 'List',
+      component: List,
+    },
+    {
+      // 말하기페이지
+      path: '/chat',
+      name: 'Chat',
+      component: () => import('../pages/chat/Chat.vue'),
+    },
   ],
 });
 
